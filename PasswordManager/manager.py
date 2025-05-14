bb6382fe6946288b1c2a1156a7beaaaf7d55f0c9
Braedon Behnke <72123302+Behnke02@users.noreply.github.com>
2025-05-13 22:23:27 -0700

    Cleaned Up Extraneous Comments
    Removed some unnecessary comments to improve code readability.
    Also included some work-in-progress code snippets for password encryption.

--- PasswordManager/manager.py
+++ PasswordManager/manager.py
@@ -1,9 +1,11 @@
 import pandas
-import string
+from string import printable
 import secrets
+import cryptography.fernet
 PASSWORD_FILE = "passwords.csv"
 COLUMN_NAMES = ['Website', 'Password', 'Current']
-PASSWORD_CHARACTERS = string.printable
+PASSWORD_CHARACTERS = printable
+THIRTY_TWO_BYTES = 256
 
 def main():
     #Read password file
@@ -11,14 +13,16 @@ def main():
         password_df = pandas.read_csv(PASSWORD_FILE, index_col=0)
     except pandas.errors.EmptyDataError:
         first_time_setup()
+        #After the csv file is created, re-read it into working dataframe
         password_df = pandas.read_csv(PASSWORD_FILE, names=COLUMN_NAMES, index_col=0)
-    #Check if file is empty (Indicates new user)
     
     #Print introductory list
     print("Welcome to PasswordManager!", end="\n\n")
     print("Please select an option from the list below:", end="\n\n")
+
+    #Main Execution Loop
     while True:
-        #Display options (Retrieve, Create, Update, Delete, Exit)
+        #Display options (Retrieve, Create, Generate, Update, Delete, Exit)
         print("Retrieve: Fetch a password you need (R)")
         print("Create: Store a new password (C)")
         print("    NEW: Generate a new password and store (G)")
@@ -26,6 +30,7 @@ def main():
         print("Delete: Delete a password entry (D)")
         print("    NOTE: If you're just changing an expired password, you can UPDATE instead and it will store any previous passwords for a website.")
         print("Exit: Exit the program (E)")
+
         #Wait for user input
         option = input("Select your option (R, C, G, U, D, E): ").upper()
         print()
@@ -41,6 +46,7 @@ def main():
             delete_entry(password_df)
         elif option == 'E':
             print("Updating password file")
+            #Password file does not update unless user ends program
             password_df.to_csv(PASSWORD_FILE, header=COLUMN_NAMES)
             break
         else:
@@ -52,7 +58,6 @@ def first_time_setup():
     if input("Are you a new user? (Y/N) ").upper() == "Y":
         print("Entering First-Time Configuring Mode")
         print("To get started, please enter your first password")
-        print("Creating new password entry")
         website = input("What website is this password associated with? ")
         password = input("Enter your password here: ")
         temp_df = pandas.DataFrame([[website, password, True]], columns=COLUMN_NAMES)
@@ -65,15 +70,14 @@ def first_time_setup():
 def retrieve_password(password_df : pandas.DataFrame) -> None:
     print("To retrieve a password for a website, please enter the website.")
     print("NOTE: Website entries are case-specific.")
+    #NOTE: Could probably force upper-case on websites in the future.
     website = input("Please enter here: ")
     
-    #general_code = input("Please enter your General-Purpose passcode to retrieve the right password")
+    #master_key = input("Please enter your General-Purpose passcode to retrieve the right password")
 
-    #Check for website match and code match
     #Retrieve all active/expired passwords associated with website
-    #Print to user
     print(password_df.query('Website == @website'))
-    #Return to main options list upon user completion
+    
     input("Press \'Enter\' to return to menu")
     print('', end="\n\n")
 
@@ -81,20 +85,19 @@ def create_entry(password_df : pandas.DataFrame) -> None:
     print("Creating new password entry")
     website = input("What website is this password associated with? ")
     password = input("Enter your password here: ")
+    #fernet = cryptography.fernet.Fernet(input("Please enter your General-Purpose passcode to encrypt the password: ").zfill(THIRTY_TWO_BYTES).encode(encoder='base64'))
+    #password = fernet.encrypt(password.encode())
 
-    #Check website conflicts
-    #Check password conflicts
-    #Add date of entry
     #Append row to dataframe
     password_df.loc[-1] = [website, password, True]
     password_df.index += 1
     password_df.sort_index()
-    #Return to main options list upon user completion
     print()
 
 def generate_entry(password_df : pandas.DataFrame):
     print("Generating a new secure password")
     website = input("What website is this password associated with? ")
+    #TODO: Enforce password requirements (Uppercase letter, number, special character, etc.)
     password = ''.join(secrets.choice(PASSWORD_CHARACTERS) for i in range(15))
     password_df.loc[-1] = [website, password, True]
     password_df.index += 1
@@ -106,8 +109,6 @@ def update_entry(password_df : pandas.DataFrame) -> None:
     website = input("What website is this password associated with? ")
     password = input("Enter your password here: ")
 
-    #Check website match
-        #If no match, display error, check if user wants to create new entry instead
     try:
         password_df.loc[password_df.Website == website, 'Current'] = False
         password_df.loc[-1] = [website, password, True]
@@ -117,9 +118,6 @@ def update_entry(password_df : pandas.DataFrame) -> None:
         print("No website matches found.")
         if input("Would you like to create a new entry? ").upper == "Y":
             create_entry(password_df)
-    #Find existing active website entry
-    #Expire entry, replace with new entry
-    #Return to main options list upon user completion
     print()
 
 def delete_entry(password_df : pandas.DataFrame) -> None:
@@ -133,10 +131,6 @@ def delete_entry(password_df : pandas.DataFrame) -> None:
             password_df.drop(password_df[password_df.Website == website].index, inplace=True)
         except KeyError:
             print("No website matches found.")
-        #Check for all matches associated with website
-        #Get final confirmation from user
-        #Delete all associated entries
-        #Return to main options list upon user completion
     else:
         print("Deletion aborted.  Returning to main options list.")
     print()
